00f006978cab799392fab1db038c6094d619391d
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-2.local>
2019-08-09 14:52:54 +0300

    test task commit

---
+++ Exercises/DataTypes/sumTwoNumbersFromPrompt.js
@@ -0,0 +1,3 @@
+const input1 = Number( prompt( 'Input first number', ( Math.random() * 100 ).toFixed( 0 ) ) );
+const input2 = Number( prompt( 'Input second number', ( Math.random() * 100 ).toFixed( 0 ) ) );
+const summmarize = ( a, b ) => console.log( a + b );
