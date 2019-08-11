278921d5200b2a24b92cecac6a952d453850b75b
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-2.local>
2019-08-11 15:40:12 +0300

    spamCheck

--- Exercises/DataTypes/checkSpam.js
+++ Exercises/DataTypes/checkSpam.js
@@ -1,6 +1,6 @@
 const checkSpam = str => {
     const spam = ['viagra', 'xxx'];
-    const check = word => str.toLowerCase().indexOf(word) >= 0;
+    const check = word => str.toLowerCase().includes(word);
     return spam.some(check);
 }
-console.log(checkSpam('innocen t xxxxxx', ['viagra', 'xxx']));
+console.log(checkSpam('mama mila ramu', ['viagra', 'xxx']));
