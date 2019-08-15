dff495e3b16ab9c72bbed76301d9450b11ad8a38
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-2.local>
2019-08-15 17:57:59 +0300

    camelize

---
+++ Exercises/DataTypes/Array/camelize.js
@@ -0,0 +1,13 @@
+const camelize = str => {
+    const firstLetterToUpperCase = (word, index) => {
+        if (index > 0) {
+            const firstLetter = word[0].toUpperCase();
+            word = firstLetter + word.slice(1);
+        }
+        return word;
+    }
+
+    return str.split('-').map(firstLetterToUpperCase).join('');
+}
+
+console.log(camelize('element-style-display'));
