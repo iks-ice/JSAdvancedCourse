2156e4df5b14bec6825a4f19cf1140cbf19e0ae9
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-3.local>
2019-08-28 15:07:20 +0300

    commit

---
+++ Exercises/DataTypes/DOM/createCalendar.js
@@ -0,0 +1,51 @@
+const createCalendar = (elem, year, month) => {
+    const mapWeekDays = new Map([[1, 'Пн'], [2, 'Вт'], [3, 'Ср'], [4, 'Чт'], [5, 'Пт'], [6, 'Сб'], [7, 'Вс']]);
+    const createElement = tag => document.createElement(tag);
+
+    const firstDay = new Date(year, month, 1).getDay();
+    const firstDayInRus = mapWeekDays.get(firstDay);
+    const lastMonthDay = new Date(year, month + 1, 0).getDate();
+
+    const createTHead = nameWeekDays => {
+        const thead = createElement('thead');
+        const days = nameWeekDays.values();
+        const tr = createElement('tr');
+        for (const day of days) {
+            const td = createElement('td');
+            td.innerHTML = day;
+            tr.append(td);
+        }
+        thead.append(tr);
+        return thead;
+    }
+
+    const createTBody = () => {
+        const tbody = createElement('tbody');
+        let isFirstDayFilled = false;
+
+        for (let day = 1; day <= lastMonthDay;) {
+            const tr = createElement('tr');
+            for (let index = 1; index <= 7; index++) {
+                const td = createElement('td');
+                if (day > lastMonthDay) {
+                    break;
+                }
+                if (mapWeekDays.get(index) === firstDayInRus) {
+                    td.innerHTML = day++;
+                    isFirstDayFilled = true;
+                } else if (isFirstDayFilled) {
+                    td.innerHTML = day++;
+                }
+                tr.append(td);
+            }
+            tbody.append(tr);
+        }
+        return tbody;
+    }
+
+    const table = document.createElement('table');
+    table.append(createTHead(mapWeekDays), createTBody());
+    elem.append(table);
+}
+const container = document.querySelector('.container');
+createCalendar(container, 2019, 6);
--- Exercises/DataTypes/DOM/highlight.js
+++ Exercises/DataTypes/DOM/highlight.js
@@ -22,3 +22,71 @@ const highlight = () => {
 
 }
 highlight();
+
+{/* <table class="js-teachers">
+<thead>
+    <tr>
+        <td>Name</td>
+        <td>Age</td>
+        <td>Gender</td>
+        <td>Status</td>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td>Ilia</td>
+        <td>30</td>
+        <td>m</td>
+        <td data-available="true">Free</td>
+    </tr>
+    <tr>
+        <td>Tim</td>
+        <td>39</td>
+        <td>m</td>
+        <td data-available="false">On duty</td>
+    </tr>
+    <tr>
+        <td>Marina</td>
+        <td>25</td>
+        <td>f</td>
+        <td data-available="true">Ready to teach</td>
+    </tr>
+    <tr>
+        <td>Klivden</td>
+        <td>42</td>
+        <td>m</td>
+        <td>Leeeroy Jenkins!</td>
+    </tr>
+    <tr>
+        <td>Ivan</td>
+        <td>17</td>
+        <td>m</td>
+        <td data-available="true">Ready to teach</td>
+    </tr>
+</tbody>
+</table> */}
+
+// table, td {
+//     border: 1px solid #cecece;
+//     padding: 10px;
+// }
+
+// thead {
+//     font-weight: 700;
+// }
+
+// .male td {
+//     background-color: #65bff3;
+// }
+
+// .female td {
+//     background-color: #932bb3;
+// }
+
+// .available td {
+//     border: 2px solid green;
+// }
+
+// .unavailable td {
+//     border: 2px solid red;
+// }
